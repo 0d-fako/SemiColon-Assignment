5272c96e23d71fc877b7ab001f9e9d65f67b8e38
0d-fako <od.fakorede@gmail.com>
2024-12-03 12:29:26 +0100

    Phase Gate

---
+++ PhaseGate/MondayPythonTask/sum_all_digits.py
@@ -0,0 +1,12 @@
+def sum_all_digits():
+    number = int(input("Provide a number between 0 and 1000 to get the sum of all digits: "))
+    if number < 0:
+        return "invalid input, number below zero"
+    total = 0
+    while number > 0:
+        remainder = number % 10
+        total += remainder
+        number //= 10
+    return total
+    
+print(sum_all_digits())
