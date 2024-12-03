17fa45e40911ce2cdd37cf14bfe6332f5fcd97bf
0d-fako <od.fakorede@gmail.com>
2024-12-03 12:43:59 +0100

    Phase Gate

--- PhaseGate/StudentGrade/StudentGradeJava/StudentGrade.java
+++ PhaseGate/StudentGrade/StudentGradeJava/StudentGrade.java
@@ -1,20 +1,27 @@
-public class StudentGrade{
-	public static void main(String... args){
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class StudentGrade {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        StudentGrade studentGrade = new StudentGrade();
+        
 
 
+	System.out.print("Welcome to the Student Grade App");
+	
+        System.out.print("Please enter how many students are in your class: ");
+        int numberOfStudents = input.nextInt();
+        System.out.print("Please enter how many subjects are taught in your class: ");
+        int numberOfSubjects = input.nextInt();
+        int[] numbers = studentGrade.getNumberOfStudentsAndSubjects(numberOfStudents, numberOfSubjects);
+        int[][] students = studentGrade.collectScoresForEveryStudentAndSubject(numbers);
+        int[][] results = studentGrade.askQuestionsForStudents(students, input);
+        studentGrade.displaySummary(results);
+        
+        
+        
+    }
 
-
-
-
-
-
-
-	}
-
-
-
-
-
-
-
-}
+   
