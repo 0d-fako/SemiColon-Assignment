de614363735202500dbf7d7f5838765359340a6c
0d-fako <od.fakorede@gmail.com>
2024-12-04 13:50:46 +0100

    Phase Gate

--- PhaseGate/ATMMachine/ATMMachineJava/ATMMachineApp.java
+++ PhaseGate/ATMMachine/ATMMachineJava/ATMMachineApp.java
@@ -6,14 +6,36 @@ public class ATMMachineApp{
 	public static void main(Strings... args){
 
 
+	Scanner input = new Scanner(System.in);
 
+	System.out.print("Are you a new or existing user?\nSelect one for New User and Select Two for Existing User")	
+	int UserType = input.nextInt();
+	if (userType = 1){};
+	else if(user == 2){};
+	else ("Please enter a valid input")
 
-
+	
 
 	}
 
 
+	public int[]{] createAccount(Scanner input){
 
 
+	}
+	
+	public void closeAccount(){
+
+
+
+	}
+	
+	public void DepositMoney(){
+
+	
+	}
+
+
+	public void TransferMoney(){}
 
 }
---
+++ PhaseGate/ATMMachine/ATMMachineJava/Account.java
@@ -0,0 +1,74 @@
+public class Account {
+	private String firstName;
+	private String lastName;
+	private String accountNumber;
+	private double balance;
+	private String pin;
+
+	public Account(String accountNumber, String pin, String accountHolderName, String firstName, String lastName) {
+        	this.accountNumber = accountNumber;
+        	this.pin = pin;
+        	this.accountHolderName = accountHolderName;
+        	this.balance = 0.0;
+		this.firstName = firstName;
+		this.lastName = lastName
+    	}
+
+
+	public String getAccountNumber(){
+		return accountNumber
+	
+	}
+
+	public boolean validatePin(String inputPin){
+		return this.pin.equals(inputPin);
+	
+	}
+
+	public void changePin(String oldPin, String newPin){
+		if(validatePin(oldPin)){
+			this.pin == newPin;
+			System.out.print("PIN changed successfully");
+		}else {
+			System.out.print("Incorrect current Pin. Pin change unsuccessful");
+		}
+
+	}
+
+	public double getBalance(){
+		return balance;
+	}
+	
+	public void deposit(double amount){
+		if (amount > 0){
+			balance += amount;
+			System.out.printf("Deposited N%.2f. New Balance: N%.2f, amount, balance");
+		} else {
+			System.out.println("Invalid deposit amount");
+		}
+	
+	}
+	
+	public boolean withdraw(double amount){
+		if (amount > 0 && amount <= balance){
+			balance -= amount;
+			System.out.printf("Withdrawn N%.2f, New balance: N%.2f, amount, balance");
+			return true;
+	
+		} else {
+			System.out.println ("Insufficient Funds or Invalid Withdrawal amount")
+			return false;
+		}
+		
+	}
+	
+	public void setfirstName (String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+
+}
