c43ccf8f4653dd384f956e016e27125f89ceaba6
0d-fako <od.fakorede@gmail.com>
2024-12-08 12:22:46 +0100

    Codewar Update

--- Codewar/codewars.py
+++ Codewar/codewars.py
@@ -2,9 +2,9 @@ def unsorted(numbers:list)->list:
 	pairs = []
 	for index in range(len(numbers)):
 		for second_index in range(len(numbers)-1):
-			if numbers[index] - numbers[second_index]== 3 or numbers[second_index] -numbers[index] == 3 :
-				if [numbers[index],numbers[second_index]] in pairs:continue
-				else:pairs.append([numbers[index],numbers[second_index]] )
+			if numbers[second_index] - numbers[index]== 3 or numbers[index] - numbers[second_index] == 3 :
+				if [numbers[second_index],numbers[index]] in pairs:continue
+				else:pairs.append([numbers[second_index],numbers[index]] )
 	return pairs
 
 
