a458ec32f93e9441d72dc5642e593cc2df4908b2
0d-fako <od.fakorede@gmail.com>
2024-12-10 05:43:07 +0100

    Tic Tac Toe

--- ChibuzorTasks/TicTacToe/TicTacToe.java
+++ ChibuzorTasks/TicTacToe/TicTacToe.java
@@ -92,7 +92,15 @@ public class TicTacToe{
 
 
 	public static void main (String... args){
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter Player 1 name (X): ");
+		String player1 = input.nextLine();
+		
+		System.out.println("Enter Player 2 name (0): ");
+		String player2 = input.nextLine();
 
+		TicTacToe game = new TicTacToe(player1,player2);
 
 
 
