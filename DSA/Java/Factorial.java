79a95b3513a1bd1c68a91b1de4dbdbf18ca99bce
0d-fako <od.fakorede@gmail.com>
2024-12-22 19:42:53 +0100

    DSA

--- DSA/Java/Factorial.java
+++ DSA/Java/Factorial.java
@@ -1,11 +1,11 @@
-import java.util.Scanner:
+import java.util.Scanner;
 
 
 public class Factorial {
 	
 	public static int fact(int number){
-		if (n==0) return 1;
-		return n*fact(n-1);
+		if (number==0) return 1;
+		return number*fact(number-1);
 
 	}
 
@@ -13,7 +13,7 @@ public class Factorial {
 	public static void main(String... arg){
 	 	Scanner input = new Scanner(System.in);
 		System.out.print("Please enter a number to find the factorial: ");
-		int n = input.nextInt():
+		int n = input.nextInt();
 		
 		int result = fact(n);
 
