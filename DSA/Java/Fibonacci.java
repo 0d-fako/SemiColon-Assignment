e5fdc278989f7dfe475fd5bd5059712588bb6e81
0d-fako <od.fakorede@gmail.com>
2024-12-22 22:46:07 +0100

    DSA

--- DSA/Java/Fibonacci.java
+++ DSA/Java/Fibonacci.java
@@ -5,18 +5,19 @@ public class Fibonacci{
 	public static int fib (int number){
 		if(number == 0) return 0;
 		else if (number == 1) return 1;
-		return fib(n-1) + fib(n-2);
+		return fib(number-1) + fib(number-2);
 	
-
-
 	
 	}
 
-	public static vid main(String... args){
+	public static void main(String... args){
 		Scanner input = new Scanner(System.in);
 
-		System.out.print();
+		System.out.println("Enter a number: ");
+		int n = input.nextInt();
 
+		int result = fib(n);
+		System.out.println("Result is : " + result);
 
 	}
 
