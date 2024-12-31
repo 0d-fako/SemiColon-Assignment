d940e11af26f79a5eef78fbb7c3c8fe0e04ced22
0d-fako <od.fakorede@gmail.com>
2024-12-31 08:50:41 +0100

    Phone Dairy

--- JavaAssignments/RandomNumber.java
+++ JavaAssignments/RandomNumber.java
@@ -1,11 +1,16 @@
-public class RandomNumber{
+import java.util.concurrent.ThreadLocalRandom;
 
-	public static void main(String[] args){
-	
-	int random = (int) Math.random(1,10);
+public class RandomNumber {
 
-	System.out.println(random);
-	
+	private static final int MIN = 1; // Minimum value for the random number
+	private static final int MAX = 10; // Maximum value for the random number
+
+	public static void main(String[] args) {
+		int randomNumber = generateRandomNumber(MIN, MAX);
+		System.out.println(randomNumber);
 	}
-	
+
+	private static int generateRandomNumber(int min, int max) {
+		return ThreadLocalRandom.current().nextInt(min, max); // Generates a random number in the range [min, max)
+	}
 }
--- JavaAssignments/TestDrivenDevelopment/firstWordCapital/FirstLetterCapital.java
+++ JavaAssignments/TestDrivenDevelopment/firstWordCapital/FirstLetterCapital.java
@@ -1,14 +1,30 @@
 public class FirstLetterCapital{
-	public String capitalize(String sentence){
+	public static String capitalize(String sentence){
 
-		for (int i = 0;i <= sentence.length; i++){
+		String[] words = sentence.split(" ");
+		StringBuilder capitalizedSentence = new StringBuilder();
 		
+		for (String word : words) {
+			if (!word.isEmpty()) {
+				capitalizedSentence.append(Character.toUpperCase(word.charAt(0))).append(word.substring(1)).append(" ");
+			}
+		}
 		
-		} 
+		return capitalizedSentence.toString().trim();
 
 
 
 	}
+
+	public static void main(String[] args){
+		System.out.println(capitalize("Great is he who sees greatness in himself"));
+
+
+
+
+	}
+
+
 
 
 
