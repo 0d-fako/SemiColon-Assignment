b7ebafdd50cd136dc21a18a780442318215c4e76
0d-fako <od.fakorede@gmail.com>
2025-01-04 10:54:54 +0100

    Refactoring

--- ChibuzorTasks/HugeInteger/HugeInteger.java
+++ ChibuzorTasks/HugeInteger/HugeInteger.java
@@ -1,9 +1,11 @@
+package HugeInteger;
+
 import java.util.ArrayList;
 
 
 public class HugeInteger{
 
-	public static ArrayList<Integer> numbers = new ArrayList<>;
+	private ArrayList<Integer> numbers = new ArrayList<>;
 
 	public HugeInteger(String numbers){
 		parse(numbers);
@@ -14,14 +16,40 @@ public class HugeInteger{
 	public void parse(String numbers){
 		if (numbers.length() > 40) {
 			throw new IllegalArgumentException("HugeInteger: Number is too long");
+		} else {
+			for(Integer number : numbers.toCharArray()){
+
+			}
 		}
-		
+
 	}
+	private
+	public String ToString(){
+		StringBuilder numberToString = new StringBuilder();
+
+		for (Integer number : this.numbers ){
+			numberToString.append(number);
+		}
+		return numberToString.toString();
+
+
+	}
+
+	public Boolean isEqualTo(HugeInteger other){
+		return this.ToString().equals(other.ToString());
+	}
+
+	public Boolean isNotEqualTo(HugeInteger other){
+		return !(this.ToString().equals(other.ToString()));
+	}
+
 	public static void main(String... args){
 		
-		int[] newHuge = {1,2,3,4,5};
+		HugeInteger hugeInteger = new HugeInteger("10");
 
-		System.out.println(Arrays.toString(newHuge));
+
+
+		System.out.println(hugeInteger.toString(number));
 
 		for (int i = 0; i < newHuge.length; i++){
 			System.out.print(newHuge[i]);
