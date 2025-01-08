ccebbd21188277c0ce6f84fa5560c9cdadef0051
0d-fako <od.fakorede@gmail.com>
2025-01-08 05:59:57 +0100

    Ceaser Cipher

--- ChibuzorTasks/CeaserCipher/CeaserCipher.java
+++ ChibuzorTasks/CeaserCipher/CeaserCipher.java
@@ -18,6 +18,16 @@ public class CeaserCipher {
 
     }
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        CeaserCipher cipher = new CeaserCipher();
+        System.out.print("Welcome to the Ceaser Cipher App");
+
+        System.out.print("Please enter the text you want to encrypt: ");
+        cipher.text = input.nextLine();
+
+        System.out.print("Please enter the shift value: ");
+        cipher.shift = input.nextInt();
+
 
 
     }
