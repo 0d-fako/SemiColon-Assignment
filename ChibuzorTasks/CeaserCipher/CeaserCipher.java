51b640e483591008e152c01ec096a7b315a28f3d
0d-fako <od.fakorede@gmail.com>
2025-01-08 05:54:23 +0100

    Ceaser Cipher

---
+++ ChibuzorTasks/CeaserCipher/CeaserCipher.java
@@ -0,0 +1,24 @@
+package CeaserCipher;
+import java.util.Scanner;
+
+public class CeaserCipher {
+    private static final int ALPHABET_LENGTH = 26;
+    private String text;
+    private int shift;
+
+    public CeaserCipher(String text, int shift) {
+        this.text = text;
+        this.shift = shift;
+    }
+
+    public static String encrypt(String text, int shift) {
+
+    }
+    public static String decrypt(String text, int shift){
+
+    }
+    public static void main(String[] args) {
+
+
+    }
+}
