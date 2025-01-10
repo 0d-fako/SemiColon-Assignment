8e43c13e92b2d0dde90e4628fa2f9627f5e4ff34
0d-fako <od.fakorede@gmail.com>
2025-01-10 14:54:55 +0100

    Hotel reservation System

---
+++ PythonClass/classWorks/Jan-2025/hotel_reservation_system/guest.py
@@ -0,0 +1,17 @@
+import random
+class Guest:
+    name = ""
+    phone_number = ""
+    email = ""
+    booking_reference = random.randint(100000, 999999)
+
+    def __init__(self, name, phone_number, email, booking_reference):
+        self.name = name
+        self.phone_number = phone_number
+        self.email = email
+        self.booking_reference = booking_reference
+
+
+
+    def get_details(self):
+        return self.name, self.phone_number, self.email, self.booking_reference
