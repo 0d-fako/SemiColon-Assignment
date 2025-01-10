8629562eb13990099acd082e81a761137da3d311
0d-fako <od.fakorede@gmail.com>
2025-01-10 20:07:19 +0100

    Hotel reservation System

---
+++ PythonClass/classWorks/Jan-2025/hotel_reservation_system/hotel_system.py
@@ -0,0 +1,15 @@
+import room
+import booking
+
+class HotelReservationSystem:
+    def __init__(self):
+        rooms[]
+        bookings[]
+        admins[]
+        festivePeriodMultiplier
+
+    Methods:
+        initializeRooms()
+        bookRoom(guestDetails, roomType, nights, festivePeriod)
+        cancelBooking(bookingReference)
+        viewA
