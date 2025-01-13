65f98ee6b27b6f63023a9f3986265e7a2e8a4d66
0d-fako <od.fakorede@gmail.com>
2025-01-13 21:10:23 +0100

    Hotel reservation System

--- PythonClass/classWorks/Jan-2025/hotel_reservation_system/guest.py
+++ PythonClass/classWorks/Jan-2025/hotel_reservation_system/guest.py
@@ -6,6 +6,24 @@ class Guest:
         self.email = email
 
 
+    def set_name(self, name: str):
+        self.name = name
+
+    def set_phone_number(self, phone_number: str):
+        self.phone_number = phone_number
+
+    def set_email(self, email: str):
+        self.email = email
+
+    def get_name(self) -> str:
+        return self.name
+
+    def get_phone_number(self) -> str:
+        return self.phone_number
+
+    def get_email(self) -> str:
+        return self.email
+
     def get_details(self) -> dict:
         return {
             "name": self.name,
@@ -14,4 +32,4 @@ class Guest:
         }
 
     def __str__(self):
-        return f"{self.name} ({self.email})"
+        return f"{self.name} \n{self.email} \n{self.phone_number}"
