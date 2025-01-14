0abdd6b4c1fa03c5a2503f7957ffdf186b28ba42
0d-fako <od.fakorede@gmail.com>
2025-01-14 23:48:09 +0100

    Functions And Array

--- PythonClass/classWorks/Jan-2025/dictionary.py
+++ PythonClass/classWorks/Jan-2025/dictionary.py
@@ -7,4 +7,22 @@ def get_dict_details():
     return details
 
 
+
+school_records = {
+     "class_1":{
+         "students":[
+             {"name": "Harry", "scores": {"Math":88,"English":76}},
+              {"name": "Solomon", "scores": {"Math":95,"English":89}},
+        ]
+     },
+     "class_2":{
+         "students": [
+             {"name": "Daniel", "scores": {"Math": 78, "English": 72}},
+              {"name": "Samuel", "scores": {"Math": 85, "English": 80}},
+        ]
+
+     }
+}
+
+
 print(get_dict_details())
