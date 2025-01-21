0a4804a0ae15ac893df13e57195ce7b59d0a0cb4
0d-fako <od.fakorede@gmail.com>
2025-01-21 08:14:25 +0100

    Javascript

--- Javascript/Jan-2025/index.js
+++ Javascript/Jan-2025/index.js
@@ -1,9 +1,11 @@
 const http = require("http");
 
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+    res.end("Hello World! Welcome to WeJapa Internships");
+});
 
 
-server.listen(3000, () => {
+server.listen(3000, 'localhost', () => {
   console.log("Server is running on port 3000");
 });
