bc321ce0e1cb57a78988ba89d6ff0d3bd3c9dd3f
0d-fako <od.fakorede@gmail.com>
2025-01-21 12:57:28 +0100

    Javascript

---
+++ DSA/Java/ReverseString.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ReverseString {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String s = sc.nextLine();
+        System.out.println("Reversed string: " + reverseString(s));
+    }
+
+    public static String reverseString(String input) {
+        StringBuilder reversed = new StringBuilder();
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed.append(input.charAt(i));
+        }
+        return reversed.toString();
+    }
+
+}
