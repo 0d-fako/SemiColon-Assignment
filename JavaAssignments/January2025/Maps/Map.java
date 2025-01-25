20b6094657fa2788b3d5ac069f54ff48f02bdf0e
0d-fako <od.fakorede@gmail.com>
2025-01-25 15:07:59 +0100

    data structures

--- JavaAssignments/January2025/Maps/Map.java
+++ JavaAssignments/January2025/Maps/Map.java
@@ -3,5 +3,5 @@ package January2025.Maps;
 public class Map {
     private String name;
     private int id;
-    
+
 }
---
+++ PythonClass/classWorks/Jan-2025/data_structures/arrays/array.py
@@ -0,0 +1,22 @@
+class Array:
+    def __init__(self, length):
+        self.length = length
+        self.data = {}
+
+    def get(self, index):
+        return self.data[index]
+
+    def push(self, item):
+        self.data[self.length] = item
+        self.length += 1
+        return self.data
+
+    def pop(self):
+        last_item = self.data[self.length - 1]
+        del self.data[self.length - 1]
+        self.length -= 1
+        return last_item
+
+    def insert(self, index, item):
+        self.data[index] = item
+
