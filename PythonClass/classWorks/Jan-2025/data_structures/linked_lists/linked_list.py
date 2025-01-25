2e922da614ce1e26de7e97786d05827a9b0c3b18
0d-fako <od.fakorede@gmail.com>
2025-01-25 15:27:25 +0100

    data structures

---
+++ PythonClass/classWorks/Jan-2025/data_structures/linked_lists/linked_list.py
@@ -0,0 +1,19 @@
+class Node:
+    def __init__(self, value, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+
+class LinkedList:
+    def __init__(self):
+        self.first_node = None
+        self.last_node = None
+
+    def append(self, value):
+        node = Node(value, None)
+        if self.first_node is None:
+            self.first_node = node
+            self.last_node = node
+        else:
+            self.last_node.next_node = node
+            self.last_node = node
