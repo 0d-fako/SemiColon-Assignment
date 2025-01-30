7dad1fec3211eb47e26d554e2acee67968f132a6
0d-fako <od.fakorede@gmail.com>
2025-01-30 01:58:07 +0100

    Account

---
+++ ChibuzorTasks/bankapp/test/bankapp/AccountTest.java
@@ -0,0 +1,19 @@
+package bankapp;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class AccountTest {
+    private Account lumiAccount;
+
+    @BeforeEach
+    public void setUp() {
+        lumiAccount = new Account();
+    }
+
+    @Test
+    public void deposit5K_balance5kTest() {
+        
+    }
+
+}
