6cc4679abe0cccc29373aa7d0d9d254dbe967120
0d-fako <od.fakorede@gmail.com>
2025-02-03 03:45:13 +0100

    Dairy

--- ChibuzorTasks/mydairy/src/mydairy/Dairy.java
+++ ChibuzorTasks/mydairy/src/mydairy/Dairy.java
@@ -1,6 +1,23 @@
 package mydairy;
 
-public class Dairy {
+import java.util.ArrayList;
 
-    
+public class Dairy {
+        private int id;
+        private String name;
+        private String pin;
+        private ArrayList<DairyEntry> dairyEntries;
+        private boolean isLocked;
+
+
+        public Dairy(String name,String pin) {
+            this.name = name;
+            this.pin = pin;
+            this.dairyEntries = new ArrayList<>();
+            this.isLocked = false;
+            this.id = 0;
+
+        }
+
+
 }
