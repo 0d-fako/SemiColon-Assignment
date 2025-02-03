3945778a3ff50ba2d72708e732260310e448aad3
0d-fako <od.fakorede@gmail.com>
2025-02-03 15:47:40 +0100

    Python Decorators

---
+++ PythonClass/classWorks/Feb-2025/OurTime.py
@@ -0,0 +1,18 @@
+class OurTime:
+    def __init__(self, hours, minutes, seconds):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+
+
+
+    @property
+    def hours(self):
+        return self_hour
+
+    
+    @hours.setter
+    def hours(self, hour):
+        if hour < 0 or hour > 23:
+            raise ValueError("hour must be between 0 and 23")
+        self_hours = hour
