532f39a7182a767868804cc5b9a748918494048e
0d-fako <od.fakorede@gmail.com>
2025-02-04 05:52:07 +0100

    Dairy

---
+++ ChibuzorTasks/mydairy/src/mydairy/Dairies.java
@@ -0,0 +1,17 @@
+package mydairy;
+
+
+import java.util.ArrayList;
+
+public class Dairies {
+    private ArrayList<Dairy> dairies;
+
+
+    public Dairies() {
+        dairies = new ArrayList<>();
+    }
+
+    public void addDairy(String name , String pin) {
+        
+    }
+}
