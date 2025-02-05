0e3a681d447a6be2a7d7f3cba5795770101448c1
0d-fako <od.fakorede@gmail.com>
2025-02-05 17:26:54 +0100

    Television

--- PythonClass/classWorks/Feb-2025/television.py
+++ PythonClass/classWorks/Feb-2025/television.py
@@ -3,12 +3,11 @@ class Television:
         self.__is_on = False
         self.__previous_volume = 0
         self.__volume_level = self.__previous_volume
-        self.__channel_level = 0
+        self.__channel_level = 1
 
 
     def turn_on(self):
         self.__is_on = True
-
 
     def turn_off(self):
         self.__is_on = False
@@ -23,3 +22,19 @@ class Television:
     def decrease_volume(self):
         if self.__is_on and self.__volume_level > 0  :
             self.__volume_level-=1
+
+
+    def increase_channel_level(self):
+        if self.__is_on and self.__channel_level < 50:
+            self.__channel_level+=1
+
+    def decrease_channel_level(self):
+        if self.__is_on and self.__channel_level > 1 : self.__channel_level-=1
+
+    def get_channel(self):
+        if self.__is_on: return self.__channel_level
+
+    def search_channel(self, channel:int):
+        if self.__is_on and 0 < channel < 50:
+            self.__channel_level = channel
+            
