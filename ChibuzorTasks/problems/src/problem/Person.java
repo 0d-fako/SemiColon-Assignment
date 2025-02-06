be772ab6b95690a44c0d4eddf85efe8a81af5545
0d-fako <od.fakorede@gmail.com>
2025-02-06 16:35:03 +0100

    Problems

--- ChibuzorTasks/problems/src/problem/Person.java
+++ ChibuzorTasks/problems/src/problem/Person.java
@@ -1,4 +1,16 @@
 package problem;
 
+import java.util.ArrayList;
+
 public class Person {
+    private String name;
+    private ArrayList<Problem> problems;
+    private int noOfProblems;
+
+
+    public Person(String name) {
+        this.name = name;
+        this.problems = new ArrayList<>();
+        this.noOfProblems = 0;
+    }
 }
