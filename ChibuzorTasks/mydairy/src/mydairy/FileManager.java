532df1d2be2fc8d7895f83cec3b7c4da24ecce5f
0d-fako <od.fakorede@gmail.com>
2025-02-08 20:46:37 +0100

    Dairy

---
+++ ChibuzorTasks/mydairy/src/mydairy/FileManager.java
@@ -0,0 +1,34 @@
+package mydairy;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class FileManager {
+    private static final String FILE_PATH = "dairies.dat";
+
+    public static void SaveDairies(Dairies dairies) {
+        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(
+                Files.newOutputStream(Paths.get(FILE_PATH)))) {
+                    objectOutputStream.writeObject(dairies);
+                    System.out.println("Dairies saved to " + FILE_PATH);
+        } catch (IOException e) {
+                    System.err.println("Error saving Dairies to " + e.getMessage());
+        }
+    }
+
+    public static Dairies LoadDairies() {
+        File file = new File(FILE_PATH);
+        if (!file.exists()) {
+            return null;
+        }
+        try (ObjectInputStream objectInputStream = new ObjectInputStream(
+                Files.newInputStream(Paths.get(FILE_PATH)))){
+                return (Dairies) objectInputStream.readObject();
+
+        }catch (IOException | ClassNotFoundException e) {
+            System.err.println("Error loading Dairies from " + FILE_PATH);
+            return null;
+        }
+    }
+}
