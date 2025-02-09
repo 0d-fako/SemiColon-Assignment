f8252ffbbe52b5a0508db20db13fe9692b35ee6c
0d-fako <od.fakorede@gmail.com>
2025-02-09 20:02:09 +0100

    Dairy

--- ChibuzorTasks/mydairy/src/mydairy/Dairy.java
+++ ChibuzorTasks/mydairy/src/mydairy/Dairy.java
@@ -11,15 +11,14 @@ public class Dairy implements Serializable {
     private ArrayList<DairyEntry> dairyEntries;
     private boolean isLocked;
 
-
-    public Dairy(String name,String pin) {
+    public Dairy(String name, String pin) {
         this.name = name;
         this.pin = pin;
         this.dairyEntries = new ArrayList<>();
         this.isLocked = false;
         this.id = 0;
-
     }
+
     public DairyEntry createDairyEntry(String title, String body) {
         if(this.isLocked) unlockDairy(pin);
         DairyEntry entry = new DairyEntry(++id, title, body);
@@ -28,10 +27,10 @@ public class Dairy implements Serializable {
         return entry;
     }
 
-
     public void unlockDairy(String pin) {
         if(validatePin(pin)) this.isLocked = false;
     }
+
     public boolean validatePin(String pin) {
         if(pin.isEmpty()) throw new RuntimeException("Pin cannot be empty");
         if(!pin.equals(this.pin)) throw new RuntimeException("Invalid pin");
@@ -40,7 +39,7 @@ public class Dairy implements Serializable {
 
     public void lockDairy(String pin) {
         if(this.isLocked) throw new RuntimeException("Locked");
-        if (validatePin(pin)) this.isLocked = true;
+        if(validatePin(pin)) this.isLocked = true;
     }
 
     public boolean isLocked() {
@@ -57,9 +56,9 @@ public class Dairy implements Serializable {
 
     public DairyEntry findDairyEntryById(int id) {
         if(dairyEntries.isEmpty()) return null;
-        if (this.isLocked) throw new RuntimeException("Locked");
-        for (DairyEntry entry : this.dairyEntries) {
-            if (entry.getId() == id) {
+        if(this.isLocked) throw new RuntimeException("Locked");
+        for(DairyEntry entry : this.dairyEntries) {
+            if(entry.getId() == id) {
                 return entry;
             }
         }
@@ -71,10 +70,10 @@ public class Dairy implements Serializable {
     }
 
     public String viewDairyEntry(String pin) {
-        if (!validatePin(pin)) throw new RuntimeException("Invalid pin");
+        if(!validatePin(pin)) throw new RuntimeException("Invalid pin");
         this.unlockDairy(pin);
         StringBuilder entries = new StringBuilder();
-        for (DairyEntry entry : this.dairyEntries) {
+        for(DairyEntry entry : this.dairyEntries) {
             entries.append("**************************");
             entries.append(entry.toString());
             entries.append("**************************");
@@ -82,8 +81,6 @@ public class Dairy implements Serializable {
         }
         return entries.toString();
     }
-
-
 
     public void updateEntryById(int id, String title, String body) {
         if(this.isLocked) throw new RuntimeException("Locked");
@@ -95,7 +92,4 @@ public class Dairy implements Serializable {
     public String getName() {
         return this.name;
     }
-
-
-
 }
