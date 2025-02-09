4f2a2185d78bef22870ea16cfe9f74f945eb6e85
0d-fako <od.fakorede@gmail.com>
2025-02-09 20:19:02 +0100

    Dairy

--- ChibuzorTasks/estore/src/estore/BillingInformation.java
+++ ChibuzorTasks/estore/src/estore/BillingInformation.java
@@ -1,4 +1,9 @@
 package estore;
 
 public class BillingInformation {
+        private String receiverPhone;
+        private String receiverName;
+        private Address deliveryAddress;
+        private CreditCardInformation cardInfo;
+    }
 }
--- ChibuzorTasks/estore/src/estore/CreditCardInformation.java
+++ ChibuzorTasks/estore/src/estore/CreditCardInformation.java
@@ -1,4 +1,10 @@
 package estore;
 
 public class CreditCardInformation {
+    private String cvv;
+    private int expirationYear;
+    private int expirationMonth;
+    private String cardNumber;
+    private String nameOnCard;
+    private CardType cardType;
 }
