aed282b9dbb50aed5784cdc1cd984defa5e688a9
0d-fako <od.fakorede@gmail.com>
2025-02-13 15:37:56 +0100

    dsa - array

---
+++ PythonClass/classWorks/Feb-2025/dsa/my_array.py
@@ -0,0 +1,28 @@
+class Array:
+    def __init__(self, size):
+        self.array = [None] * size
+
+    def is_empty(self):
+        for x in self.array:
+            if x is not None:
+                return False
+        return True
+
+    def get_item(self, index):
+        if 0 <= index < len(self.array):
+            return self.array[index]
+        else:
+            raise IndexError("Array index out of bounds")
+
+    def set_item(self, index, value):
+        if 0 <= index < len(self.array):
+            self.array[index] = value
+        else:
+            raise IndexError("Array index out of bounds")
+
+    def size(self):
+        return len(self.array)
+
+    def to_string(self):
+        return str(self.array)
+
