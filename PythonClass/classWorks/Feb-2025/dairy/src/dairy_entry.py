50f62c099776c5de4b3e7078b6037f778be70c67
0d-fako <od.fakorede@gmail.com>
2025-02-13 14:14:33 +0100

    Diary in python

--- PythonClass/classWorks/Feb-2025/dairy/src/dairy_entry.py
+++ PythonClass/classWorks/Feb-2025/dairy/src/dairy_entry.py
@@ -1,8 +1,33 @@
-import D
+import datetime
 
-class  DairyEntry:
+class DairyEntry:
     def __init__(self, id, title, body):
-        self.__id = id
-        self.__title = title
-        self.__body = body
-        self.__id = l
+        self.id = id
+        self.set_title(title)
+        self.set_body(body)
+        self.date = datetime.date.today()
+
+    def get_id(self):
+        return self.id
+
+    def set_title(self, title):
+        if not title:
+            raise ValueError("Title cannot be null or empty")
+        self.title = title
+
+    def get_title(self):
+        return self.title
+
+    def set_body(self, body):
+        if not body:
+            raise ValueError("Body cannot be null or empty")
+        self.body = body
+
+    def get_date(self):
+        return self.date
+
+    def get_body(self):
+        return self.body
+
+    def __str__(self):
+        return f"Date: {self.date}\nTitle: {self.title}\nBody: {self.body}\n"
