b11bdd9c5d1d91a855c18d65f2496f347cd5f86f
0d-fako <od.fakorede@gmail.com>
2025-02-17 17:38:49 +0100

    Array Kata

---
+++ ChibuzorTasks/turtle/test/turtle/SevenSegmentDisplayTest.java
@@ -0,0 +1,17 @@
+package turtle;
+
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions.*;
+
+public class SevenSegmentDisplayTest {
+    private SevenSegmentDisplay sevenSegmentDisplay;
+
+
+    @BeforeEach
+    public void setUp() {
+        sevenSegmentDisplay = new SevenSegmentDisplay();
+    }
+}
