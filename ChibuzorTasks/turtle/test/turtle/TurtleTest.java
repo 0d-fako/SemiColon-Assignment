a7d7eacb359f957626c471f9141e36b8a164aae5
0d-fako <od.fakorede@gmail.com>
2025-02-17 14:29:51 +0100

    Turtle

--- ChibuzorTasks/turtle/test/turtle/TurtleTest.java
+++ ChibuzorTasks/turtle/test/turtle/TurtleTest.java
@@ -8,4 +8,13 @@ public class TurtleTest {
     private Turtle ijapa;
 
 
+    @BeforeEach
+    public void setUp() {
+        ijapa = new Turtle();
+    }
+
+
+    @Test
+    public void testTurtle() {
+    }
 }
