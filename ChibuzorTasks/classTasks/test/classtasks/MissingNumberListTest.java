753f0a177bdc2597e57c916e57f284467d98c621
0d-fako <od.fakorede@gmail.com>
2025-02-26 10:40:21 +0100

    Missing Number List

---
+++ ChibuzorTasks/classTasks/test/classtasks/MissingNumberListTest.java
@@ -0,0 +1,43 @@
+package classtasks;
+
+import classTasks.src.MissingNumberList;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+public class MissingNumberListTest {
+    private MissingNumberList list;
+
+    @BeforeEach
+    public void setUp() {
+        list = new MissingNumberList();
+    }
+
+
+    @Test
+    public void MissingNumberListWorksForSortedArrayTest() {
+        int[] numbersArray = {1, 2, 5,7,9};
+        ArrayList<Integer> result = list.missingNumberList(numbersArray);
+        ArrayList<Integer> output = new ArrayList<>(Arrays.asList(3, 4, 6, 8));
+        assertEquals(result, output);
+
+
+    }
+
+
+    @Test
+    public void missingNumberListWorksForUnsortedArrayTest() {
+        int[] numbersArray = {5, 1, 2};
+        ArrayList<Integer> result = list.missingNumberList(numbersArray);
+        ArrayList<Integer> output = new ArrayList<>(Arrays.asList(3, 4));
+        assertEquals(result, output);
+    }
+
+
+}
+
