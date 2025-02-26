17fe1956168091551532b7d967862a64a9d18e77
0d-fako <od.fakorede@gmail.com>
2025-02-26 15:36:52 +0100

    ArrayTasks

--- ChibuzorTasks/classTasks/src/classtasks/Multiples.java
+++ ChibuzorTasks/classTasks/src/classtasks/Multiples.java
@@ -1,4 +1,29 @@
 package classTasks.src.classtasks;
 
+import java.util.ArrayList;
+
 public class Multiples {
+    public ArrayList<Integer> multiples(int startingNumber, int endingNumber, int multiple) {
+        ArrayList<Integer> multiples = new ArrayList<>();
+        for (int i = startingNumber; i <= endingNumber; i++) {
+            if (i % multiple == 0) {
+                multiples.add(i);
+            }
+        }
+        return multiples;
+    }
+
+    public int[] multiplesArray(int startingNumber, int endingNumber, int multiple) {
+        int size = (endingNumber - startingNumber) / multiple + 1;
+        int[] result = new int[size];
+        int index = 0;
+
+        for (int i = startingNumber; i <= endingNumber; i++) {
+            if (i % multiple == 0) {
+                result[index++] = i;
+            }
+        }
+        return result;
+    }
+
 }
--- ChibuzorTasks/classTasks/test/classtasks/MultiplesTest.java
+++ ChibuzorTasks/classTasks/test/classtasks/MultiplesTest.java
@@ -1,4 +1,50 @@
 package classTasks.test.classtasks;
 
+import classTasks.src.classtasks.Multiples;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MultiplesTest {
+    private Multiples multiples;
+
+
+
+    @BeforeEach
+    public void setUp() {
+        multiples = new Multiples();
+    }
+
+
+    @Test
+    public void multipleWorksCorrectlyTest() {
+        int startingNumber = 5;
+        int endingNumber = 50;
+        int multiple = 5;
+
+        ArrayList<Integer> result = multiples.multiples(startingNumber, endingNumber, multiple);
+        ArrayList<Integer> output = new ArrayList<>(Arrays.asList(5, 10, 15, 20, 25, 30, 35, 40, 45, 50));
+        Assertions.assertEquals(output, result);
+
+
+    }
+
+    @Test
+    public void multipleArrayWorksCorrectlyTest() {
+        int startingNumber = 5;
+        int endingNumber = 50;
+        int multiple = 5;
+
+        int[] result = multiples.multiplesArray(startingNumber, endingNumber, multiple);
+        int[] output = {5, 10, 15, 20, 25, 30, 35, 40, 45, 50};
+        Assertions.assertArrayEquals(output, result);
+
+
+    }
+
+
 }
