cc1cfb902c889f0ee7487b00605a8bb7b5aeedd8
0d-fako <od.fakorede@gmail.com>
2025-02-26 10:14:38 +0100

    Missing Number List

---
+++ ChibuzorTasks/classTasks/MissingNumberList.java
@@ -0,0 +1,28 @@
+package classTasks;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MissingNumberList{
+
+    public ArrayList<Integer> missingNumberList(int[] numbersArray) {
+        ArrayList<Integer> missingNumbers = new ArrayList<>();
+        Arrays.sort(numbersArray);
+        int smallest = numbersArray[0];
+        int largest = numbersArray[numbersArray.length - 1];
+        int currentIndex = 0;
+
+        while (smallest <= largest) {
+            if (currentIndex < numbersArray.length && numbersArray[currentIndex] == smallest) {
+                currentIndex++;
+            } else {
+                missingNumbers.add(smallest);
+            }
+            smallest++;
+        }
+
+        return missingNumbers;
+    }
+
+
+}
