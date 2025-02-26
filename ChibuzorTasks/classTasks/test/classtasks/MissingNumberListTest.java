ee775644d50dd84b49f6c418676a392b0865aa34
0d-fako <od.fakorede@gmail.com>
2025-02-26 10:46:35 +0100

    Missing Number List

--- ChibuzorTasks/classTasks/test/classtasks/MissingNumberListTest.java
+++ ChibuzorTasks/classTasks/test/classtasks/MissingNumberListTest.java
@@ -1,6 +1,5 @@
 package classtasks;
 
-import classTasks.src.MissingNumberList;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
