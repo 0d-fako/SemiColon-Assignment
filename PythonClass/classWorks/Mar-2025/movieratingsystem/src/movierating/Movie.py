b9d1e451d4f47d91a8edc908aa72a9267356575b
0d-fako <od.fakorede@gmail.com>
2025-03-17 10:35:15 +0100

    Created Movie Class

--- PythonClass/classWorks/Mar-2025/movieratingsystem/src/movierating/Movie.py
+++ PythonClass/classWorks/Mar-2025/movieratingsystem/src/movierating/Movie.py
@@ -3,9 +3,25 @@ from datetime import datetime
 
 class Movie:
     def __init__(self, title):
-        self.title = title
+        self.movie_title = title
         self.movie_date = datetime.now()
         self.rating = []
 
 
 
+    def get_movie_title(self):
+        return self.movie_title
+
+    def get_movie_date(self):
+        return self.movie_date
+
+    def get_average_rating(self):
+        if self.rating is None: return 0
+        return sum(self.rating) / len(self.rating)
+
+    def to_dict(self):
+        return {
+            'Movie Title': self.movie_title,
+            'Movie Date': self.movie_date,
+            'Average Rating': self.get_average_rating()
+        }
