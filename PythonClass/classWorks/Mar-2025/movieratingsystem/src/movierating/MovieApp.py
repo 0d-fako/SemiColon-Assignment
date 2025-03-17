ddfc520ca6d265a14f7bd898d2824f49e3da728e
0d-fako <od.fakorede@gmail.com>
2025-03-17 11:11:58 +0100

    first version done

--- PythonClass/classWorks/Mar-2025/movieratingsystem/src/movierating/MovieApp.py
+++ PythonClass/classWorks/Mar-2025/movieratingsystem/src/movierating/MovieApp.py
@@ -7,22 +7,29 @@ class MovieApp:
 
     def create_new_movie(self, movie_title):
         if movie_title in self.movies:
-            return "Movie already exists"
+            raise NameError("Movie '{}' already exists".format(movie_title))
         movie = Movie(movie_title)
         self.movies[movie_title] = movie
 
 
     def rate_movie(self, movie_title, movie_rating):
+        if movie_title not in self.movies:
+            raise NameError("Movie '{}' doesn't exist".format(movie_title))
         movie = self.movies[movie_title]
         movie.rating.append(movie_rating)
 
 
     def view_average_rating(self, movie_title):
+        if len(self.movies) == 0:
+            raise NameError("No Movies exists")
         movie = self.movies[movie_title]
         rating = movie.get_average_rating()
+        return rating
 
 
     def view_all_average_rating(self):
+        if len(self.movies) == 0:
+            raise NameError("No Movies exists")
         for movie in self.movies:
             movie = self.movies[movie]
             rating = movie.get_average_rating()
--- PythonClass/classWorks/Mar-2025/movieratingsystem/src/movierating/MovieMain.py
+++ PythonClass/classWorks/Mar-2025/movieratingsystem/src/movierating/MovieMain.py
@@ -8,21 +8,21 @@ def main():
         print("1. Add A Movie")
         print("2. Rate A Movie")
         print("3. Get Average Rating")
-        print("4. View All Movies")
+        print("4. View All Movies ratings")
         print("5. Exit")
         choice = input("Enter choice: ")
 
         if choice == '1':
-            movie_title = input("Please enter : ")
-            app.create_new_movie()
+            movie_title = input("Please enter new movie name: ")
+            app.create_new_movie(movie_title)
         elif choice == '2':
             movie_title = input("Please enter a movie title to rate: ")
-
-            app.rate_a_movie(movie_title)
+            app.rate_movie(movie_title)
         elif choice == '3':
-            StudentDashboard.enroll_in_course(student)
+            movie_title = input("Please enter a movie to view rating: ")
+            app.view_average_rating(movie_title)
         elif choice == '4':
-            StudentDashboard.view_course_grades(student)
+            app.view_all_average_rating()
         elif choice == '5':
             break
         else:
