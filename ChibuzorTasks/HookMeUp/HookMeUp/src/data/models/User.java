e6660e1a99a22d797dc009a1b8a29fa8e6d88073
0d-fako <od.fakorede@gmail.com>
2025-03-24 01:44:23 +0100

    Hook me up app

--- ChibuzorTasks/HookMeUp/HookMeUp/src/data/models/User.java
+++ ChibuzorTasks/HookMeUp/HookMeUp/src/data/models/User.java
@@ -1,4 +1,68 @@
 package data.models;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class User {
+    private int id;
+    private String username;
+    private String password;
+    private LocalDateTime dob;
+    private String gender;
+    private List<String> interests;
+
+    public User(String username, String password, LocalDateTime dob, String gender) {
+        this.username = username;
+        this.password = password;
+        this.dob = dob;
+        this.gender = gender;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<String> getInterests() {
+        return interests;
+    }
+
+    public void setInterests(List<String> interests) {
+        this.interests = interests;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public LocalDateTime getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDateTime dob) {
+        this.dob = dob;
+    }
 }
