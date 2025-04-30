c1d15542932912a2780fa168b118daaf3ac2c579
0d-fako <od.fakorede@gmail.com>
2025-04-30 23:59:44 +0100

    Updates

--- ChibuzorTasks/HookMeUp/HookMeUp/src/services/UserServices.java
+++ ChibuzorTasks/HookMeUp/HookMeUp/src/services/UserServices.java
@@ -1,4 +1,8 @@
 package services;
 
 public interface UserServices {
+
+    public boolean login(String username, String password);
+
+    public boolean register(String username, String password);
 }
