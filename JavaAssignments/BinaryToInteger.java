83559d48717985bb7f57962241650256e4543ec8
0d-fako <od.fakorede@gmail.com>
2025-05-09 09:39:53 +0100

    Updated Implement collections and comparable

--- JavaAssignments/BinaryToInteger.java
+++ JavaAssignments/BinaryToInteger.java
@@ -7,19 +7,19 @@ public class BinaryToInteger {
 
 	System.out.print("Enter a Binary Number: ");
 	int binaryToConvert = input.nextInt();
-	
-	
-	
-	int convertedInteger = 0;
-	int counter = 0;
-	
-	while (binaryToConvert != 0){
-		int rem = binaryToConvert % 10;
-		double remPowered =  rem * Math.pow(2,counter);
-		binaryToConvert /=   10;
-		convertedInteger  += remPowered ;
-		counter++;
-	}
+		int convertedInteger = 0;
+		int counter = 0;
+
+		while (binaryToConvert != 0){
+			int rem = binaryToConvert % 10;
+			double remPowered =  rem * Math.pow(2,counter);
+			binaryToConvert /=   10;
+			convertedInteger  += remPowered ;
+			counter++;
+
+
+
+		}
 	
 
 	System.out.printf("Decimal number is %s", convertedInteger );
