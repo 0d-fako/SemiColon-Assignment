cf3d2f0512bed223d015fdafe1d9e7d5a6d8b093
0d-fako <od.fakorede@gmail.com>
2025-05-09 15:40:11 +0100

    File Management

--- ChibuzorTasks/classTasks/src/classtasks/FilePractice.java
+++ ChibuzorTasks/classTasks/src/classtasks/FilePractice.java
@@ -1,8 +1,23 @@
 package classtasks;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class FilePractice {
 
-    public void createFile(String filePath) {
+    public static void createFile(String filePath) {
         if (filePath == null || filePath.isEmpty()) throw new IllegalArgumentException("File path cannot be null or empty");
     }
+
+    public static File findFileByPath(String filePath) {
+        Path foundPath = Paths.get(filePath);
+        return Files.exists(foundPath) ? foundPath.toFile() : null;
+    }
+
+    public static void writeToFile(String filePath, String content) {
+
+    }
 }
+
--- ChibuzorTasks/classTasks/test/classtasks/FilePracticeTest.java
+++ ChibuzorTasks/classTasks/test/classtasks/FilePracticeTest.java
@@ -1,5 +1,7 @@
 package classtasks;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
@@ -8,12 +10,35 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FilePracticeTest {
 
+    private String path;
+
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+
+    @AfterEach
+    void tearDown() {
+
+    }
+
+
     @ParameterizedTest
     @NullAndEmptySource
     void testThatCreateFileIfFilePathNullOrEmptyThrowsException (String filePath) {
         FilePractice filePractice = new FilePractice();
         assertThrows( IllegalArgumentException.class, () -> filePractice.createFile(filePath));
     }
+
+    @Test
+    void testThatDuplicateFileNameThrowsException () {
+        FilePractice filePractice = new FilePractice();
+        assertThrows( IllegalArgumentException.class, () -> filePractice.createFile(path));
+    }
+
+
 
 
 
