0aa42764f41e149c707e496736350a1db9f2ca0c
0d-fako <od.fakorede@gmail.com>
2025-06-04 16:42:22 +0100

    Base Converter

--- ChibuzorTasks/classTasks/baseconverter/src/baseconverter/BaseConverter.java
+++ ChibuzorTasks/classTasks/baseconverter/src/baseconverter/BaseConverter.java
@@ -3,19 +3,19 @@ package baseconverter;
 import java.util.Scanner;
 
 public class BaseConverter {
-    public int baseConverter(int base, int numberToConvert) {
+    public int baseConverter(int newBase, int numberToConvert, int currentBase) {
         int convertedInteger = 0;
         int counter = 0;
 
-        while (numberToConvert != 0){
-            int rem = numberToConvert % 10;
-            double remPowered =  rem * Math.pow(base,counter);
-            numberToConvert /=   10;
-            convertedInteger  += remPowered ;
+        while (numberToConvert != 0) {
+            int rem = numberToConvert % newBase;
+            double remPowered = rem * Math.pow(currentBase, counter);
+            numberToConvert /= 10;
+            convertedInteger += remPowered;
             counter++;
-        
 
-
+        }
+        return convertedInteger;
     }
 }
 
--- ChibuzorTasks/classTasks/baseconverter/test/baseconverter/BaseConverterTest.java
+++ ChibuzorTasks/classTasks/baseconverter/test/baseconverter/BaseConverterTest.java
@@ -4,16 +4,22 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertEquals;
+
+
 public class BaseConverterTest {
 
     @Test
     public void baseConverterTest() {
-        int numberToConvert = 111111;
-        int base  = 10;
-
+        int numberToConvert = 110;
+        int newBase  = 10;
+        int convertedInterger = 6;
+        int currentBase = 2;
         BaseConverter converter = new BaseConverter();
 
-        converter.baseConverter(base, numberToConvert);
+        int ExpectedResult = converter.baseConverter(newBase, numberToConvert, currentBase);
+        assertEquals(convertedInterger, ExpectedResult);
+
 
 
     }
